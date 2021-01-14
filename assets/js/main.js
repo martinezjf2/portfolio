ee9acc2044417b20c17612e138373835c8b56cfa
Jeffrey Martinez <martinezjf2@gmail.com>
2021-01-13 21:22:17 -0500

    just kept one picture as the main

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,5 @@
 
-function randombg(){
-  var random= Math.floor(Math.random() * 6) + 0;
-  var bigSize = ["url('assets/img/coverpicture/hero-bg.jpg')",
-              "url('assets/img/coverpicture/hero-two.jpg')", 
-              "url('assets/img/coverpicture/hero-four.jpg')",
-              "url('assets/img/coverpicture/hero-six.jpg')",
-              "url('assets/img/coverpicture/hero-seven.jpg')",
-              "url('assets/img/coverpicture/hero-nine.jpg')"
-            ];
-  document.getElementById("hero").style.backgroundImage=bigSize[random];
-};
+
 
 
 !(function($) {
